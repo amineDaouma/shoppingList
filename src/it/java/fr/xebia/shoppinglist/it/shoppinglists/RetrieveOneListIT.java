330a98850b11898aa45dd91dd663e2f506e0ee82
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-03-04 22:04:42 +0100

    Add REST resources to retrieve a list and add a product to a list

--- src/it/java/fr/xebia/shoppinglist/it/shoppinglists/RetrieveOneListIT.java
+++ src/it/java/fr/xebia/shoppinglist/it/shoppinglists/RetrieveOneListIT.java
@@ -1,16 +1,18 @@
 package fr.xebia.shoppinglist.it.shoppinglists;
 
 import static com.jayway.restassured.RestAssured.given;
-import static com.jayway.restassured.RestAssured.when;
 import static com.jayway.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.equalTo;
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.emptyIterable;
 import org.junit.Test;
 import fr.xebia.shoppinglist.users.User;
 
-public class RetrieveAllListsIT {
+public class RetrieveOneListIT {
 
     @Test
-    public void should_add_one_new_list_to_an_existing_user() {
+    public void
+    should_add_one_product_to_a_list() {
         given().
                 body(new User("test@test.fr", "norman", "password")).
                 contentType(JSON).
@@ -21,7 +23,7 @@ public class RetrieveAllListsIT {
         ;
 
         given().
-                body("Apero tonight").
+                body("Romantic dinner").
                 contentType(JSON).
         when().
                 post("/api/users/1/lists").
@@ -29,13 +31,20 @@ public class RetrieveAllListsIT {
                 statusCode(201)
         ;
 
+        given().
+                contentType(JSON).
         when().
-                get("/api/users/1/lists").
+                get("/api/users/1/lists/1").
         then().
                 statusCode(200).
-                body(equalTo("[{\"title\":\"Apero tonight\",\"id\":1,\"products\":[]}]"))
+                body(matchesJsonSchemaInClasspath("schemas/list.json")).
+                body("id", equalTo(1)).
+                body("title", equalTo("Romantic dinner")).
+                body("products", emptyIterable())
         ;
 
+        given().
+                contentType(JSON).
         when().
                 delete("/api/users/1").
         then().
