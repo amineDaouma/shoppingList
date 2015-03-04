b1b3df965db214d90bbc054a170f47b99a8a8103
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-03-04 21:49:06 +0100

    Add a controller that adds a new product to a list

--- src/test/javascript/listControllerSpec.js
+++ src/test/javascript/listControllerSpec.js
@@ -25,4 +25,18 @@ describe('Manage list controller', function() {
         expect(scope.list).toEqual(list);
         expect(scope.newProduct).toBeDefined();
     });
+
+    it('should add a product and notify the server', inject(function() {
+        scope.newProduct = "salad";
+        httpBackend
+            .whenPOST('/api/users/1/lists/12/products', scope.newProduct)
+            .respond(201, scope.newProduct);
+
+        scope.addProductToList();
+
+        httpBackend.flush();
+        httpBackend.expectPOST('/lists/12/products');
+        expect(scope.list.products).toContain("salad");
+        expect(scope.newProduct).toBe("");
+    }));
 });
