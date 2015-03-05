57f1c5346d886493bfcb65b35dd8feeb71e4d4e7
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-03-05 16:21:48 +0100

    Split maven project into maven modules with web and bdd (bdd is broken)

---
+++ web/src/test/java/io/pvardanega/shoppinglist/users/shoppinglist/AddProductIT.java
@@ -0,0 +1,41 @@
+package io.pvardanega.shoppinglist.users.shoppinglist;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.http.ContentType.JSON;
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import org.junit.Rule;
+import org.junit.Test;
+import io.pvardanega.shoppinglist.config.ListRule;
+
+public class AddProductIT {
+
+    @Rule
+    public ListRule listRule = new ListRule();
+
+    @Test
+    public void should_add_one_product_to_a_list() {
+        given().
+                body("Salad").
+                contentType(JSON).
+        when().
+                post("/api/users/1/lists/1/products").
+        then().
+                statusCode(200).
+                body(equalTo("Salad"))
+        ;
+
+        given().
+                contentType(JSON).
+        when().
+                get("/api/users/1/lists/1").
+        then().
+                statusCode(200).
+                body(matchesJsonSchemaInClasspath("schemas/list.json")).
+                body("id", equalTo(1)).
+                body("name", equalTo("Romantic dinner")).
+                body("products", hasItems("Salad"))
+        ;
+    }
+}
