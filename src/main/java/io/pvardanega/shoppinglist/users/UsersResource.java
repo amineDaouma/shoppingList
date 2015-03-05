11c59aa33284ebf16fe373534546c77c04e7f429
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-03-05 11:43:29 +0100

    Refactor with java 8 streams

--- src/main/java/io/pvardanega/shoppinglist/users/UsersResource.java
+++ src/main/java/io/pvardanega/shoppinglist/users/UsersResource.java
@@ -3,6 +3,9 @@ package io.pvardanega.shoppinglist.users;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.ok;
+import static javax.ws.rs.core.Response.status;
+import java.util.Optional;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -31,53 +34,41 @@ public class UsersResource {
                                      @PathParam("listId") Long listId,
                                      String product) {
         User user = userRepository.get(userId);
-        for (ShoppingList list : user.lists) {
-            if (listId.equals(list.id)) {
-                list.addProduct(product);
-            }
-        }
-        return Response.
-                ok().
-                entity(product).
-                build()
-                ;
+        user.lists.
+            stream().
+            filter(list -> listId.equals(list.id)).
+            findFirst().
+            ifPresent(list -> list.addProduct(product));
+        return ok(product).build();
     }
 
     @POST
     public Response createUser(User user) {
-        return Response.
-                ok().
-                status(CREATED).
-                entity(userRepository.create(user)).
-                build()
-        ;
+        return ok(userRepository.create(user)).status(CREATED).build();
     }
 
     @Path("{userId}")
     @DELETE
     public Response removeUser(@PathParam("userId") Long userId) {
         userRepository.remove(userId);
-        return Response.ok().build();
+        return ok().build();
     }
 
     @Path("{userId}/lists")
     @POST
     public Response addNewList(@PathParam("userId") Long userId, String listName) {
         User user = userRepository.get(userId);
+
         ShoppingList shoppingList = new ShoppingList(user.lists.size() + 1L, listName);
         user.lists.add(shoppingList);
-        return Response.
-                ok().
-                status(CREATED).
-                entity(shoppingList).
-                build()
-        ;
+
+        return ok(shoppingList).status(CREATED).build();
     }
 
     @Path("{userId}/lists")
     @GET
     public Response retrieveAllLists(@PathParam("userId") Long userId) {
-        return Response.
+        return
                 ok().
                 entity(userRepository.get(userId).lists).
                 build();
@@ -88,18 +79,14 @@ public class UsersResource {
     public Response retrieveList(@PathParam("userId") Long userId,
                                  @PathParam("listId") Long listId) {
         User user = userRepository.get(userId);
-        for (ShoppingList list : user.lists) {
-            if (listId.equals(list.id)) {
-                return Response.
-                        ok().
-                        entity(list).
-                        build()
-                        ;
-            }
+        Optional<ShoppingList> listFound = user.lists.
+                                            stream().
+                                            filter(list -> listId.equals(list.id)).
+                                            findFirst();
+
+        if (listFound.isPresent()) {
+            return ok(listFound.get()).build();
         }
-        return Response.
-                status(NOT_FOUND).
-                build()
-                ;
+        return status(NOT_FOUND).build();
     }
 }
